dc0879b77ab0a991dcdb19eea5c3fa82a4396151
Ellen Conceição S. Barbosa <140675288+EllenBarbosa26@users.noreply.github.com>
2024-01-18 09:18:03 +0800

    Adicionei os controller

--- src/main/java/Controller/ModeradorController.java
+++ src/main/java/Controller/ModeradorController.java
@@ -1,4 +1,13 @@
 package Controller;
 
-public class ModeradorController {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ModeradorController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 }
--- src/main/java/Controller/VideoController.java
+++ src/main/java/Controller/VideoController.java
@@ -1,4 +1,14 @@
 package Controller;
 
-public class VideoController {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class VideoController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
